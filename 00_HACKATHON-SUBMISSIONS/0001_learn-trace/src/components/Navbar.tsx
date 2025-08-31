df332ed08da4e73324f0d12e963cdf964767e243
root <root@DELL-INSPIRON-15.localdomain>
2025-08-31 14:42:06 +0000

    submission ready-3

--- 00_HACKATHON-SUBMISSIONS/0001_learn-trace/src/components/Navbar.tsx
+++ 00_HACKATHON-SUBMISSIONS/0001_learn-trace/src/components/Navbar.tsx
@@ -2,8 +2,6 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
-// import Image from "next/image";
-// import logo from "@/public/logo1.png";
 
 export default function Navbar() {
   const router = useRouter();
@@ -11,10 +9,10 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-8 py-4 bg-white shadow-sm">
       {/* Logo */}
-      {/* <div className="text-xl font-bold text-blue-600 flex">
-        <Image src={logo} alt="" width={40} height={40} />
+      <div className="text-xl font-bold text-blue-600 flex">
+        {/* <Image src={logo} alt="" width={40} height={40} /> */}
         <div className="pt-1">LearnTrace</div>
-      </div> */}
+      </div>
 
       {/* Menu */}
       <ul className="flex items-center gap-6 text-gray-700">
