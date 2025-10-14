6211a961b9c1453f8b434ba0ba6e097e7355049f
Simon Reyes <reyesimon31@gmail.com>
2025-10-14 21:27:17 +0800

    Login Page

---
+++ 00_HACKATHON-SUBMISSIONS/0000_RunKada/src/screens/Login/Login.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/ui/button";
+
+export const Login = () => {
+  return (
+    <div className="bg-white min-h-screen w-full p-8">
+      <header className="mb-8">
+        <Link to="/">
+          <img
+            className="h-[120px] w-auto"
+            alt="Logofull"
+            src="https://c.animaapp.com/mgqjxiy6qqDflS/img/logofull-1.svg"
+          />
+        </Link>
+      </header>
+
+      <div className="max-w-4xl mx-auto">
+        <h1 className="[font-family:'Porter_Sans_Block-Regular',Helvetica] font-normal text-[#56504a] text-6xl mb-8">
+          Log In
+        </h1>
+        
+        <p className="[font-family:'Poppins',Helvetica] font-normal text-black text-lg mb-8">
+          Sign in to your Runkada account.
+        </p>
+
+        <Link to="/">
+          <Button className="bg-[#f7e2c6] hover:bg-[#fcd96b] text-[#56504a] rounded-[30px]">
+            <span className="[font-family:'Porter_Sans_Block-Block',Helvetica] font-normal text-base">
+              Back to Home
+            </span>
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
